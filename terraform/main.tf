f2cb0c8db7114eb2adb9f29adcd12ca0c8ce5aa5
nomadoor <nomadoor@gmail.com>
2025-09-18 10:52:57 +0900

    terraformファイル作成

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+﻿terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.api_token
+}
+
+resource "cloudflare_pages_project" "hello_world" {
+  account_id        = var.account_id
+  name              = var.project_name
+  production_branch = var.production_branch
+
+  build_config {
+    build_command   = ""
+    destination_dir = "."
+    root_dir        = ""
+  }
+
+  deployment_configs {
+    production {}
+    preview    {}
+  }
+
+  source {
+    type = var.pages_source_type
+
+    config {
+      owner             = var.repo_owner
+      repo_name         = var.repo_name
+      production_branch = var.production_branch
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "custom" {
+  count = var.custom_domain == "" ? 0 : 1
+
+  account_id   = var.account_id
+  project_name = cloudflare_pages_project.hello_world.name
+  domain       = var.custom_domain
+}
